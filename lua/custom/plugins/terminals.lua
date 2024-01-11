d0fef14b2af7ea65595d548353fdb415d78d4fe0
Ty Davis <tydavisxc2019@gmail.com>
2024-01-11 13:40:55 -0700

    Added extra keymap for terminal mode

--- lua/custom/plugins/terminals.lua
+++ lua/custom/plugins/terminals.lua
@@ -1,5 +1,6 @@
 -- vim.keymap.set('t', '<C-m>', 'maybe so', { noremap = true })
-vim.keymap.set('t', '<Esc>', '<C-\\><C-n>', { noremap = true})
+vim.keymap.set('t', '<Esc>', '<C-\\><C-n>', { noremap = true })
+vim.keymap.set('t', '<C-w>', '<C-\\><C-n><C-w>', { noremap = true })
 
 local function open_terminal()
   local height = vim.api.nvim_win_get_height(0)
