69dfd6701f987bfb21fc3f4e3ef28723aada0da2
Ty Davis <tydavisxc2019@gmail.com>
2024-01-12 22:47:49 -0700

    Got the terminal commands in a decent spot

--- lua/custom/plugins/terminals.lua
+++ lua/custom/plugins/terminals.lua
@@ -7,29 +7,8 @@ vim.keymap.set('t', '<C-k>', '<C-w>k', { noremap = true })
 vim.keymap.set('t', '<C-l>', '<C-w>l', { noremap = true })
 
 
-local function open_terminal()
-  local height = vim.api.nvim_win_get_height(0)
-  vim.cmd(height - 15 .. ' split')
-  vim.cmd('wincmd j')
-  vim.cmd('terminal')
-end
-
-vim.api.nvim_create_user_command('Term', open_terminal, {})
-
-local function open_terminal_2()
-  local height = vim.api.nvim_win_get_height(0)
-  vim.cmd(height - 15 .. ' split')
-  vim.cmd('wincmd j')
-  vim.cmd('vs')
-  vim.cmd('terminal')
-  vim.cmd('wincmd l')
-  vim.cmd('terminal')
-end
-
-vim.api.nvim_create_user_command('Term2', open_terminal_2, {})
-
 local function terminal_below(count)
-  height = vim.api.nvim_win_get_height(0)
+  local height = vim.api.nvim_win_get_height(0)
   vim.cmd(height - 15 .. ' split')
   vim.cmd('wincmd j')
   for i=1,count do
@@ -43,12 +22,35 @@ local function terminal_below(count)
   end
 end
 
-local function test_stuff(params)
-  print(params)
+vim.api.nvim_create_user_command('Term', function (args)
+  terminal_below(1)
+  end, {nargs="*"})
+
+vim.api.nvim_create_user_command('Term2', function (args)
+  terminal_below(2)
+  end, {nargs="*"})
+
+
+local function v_terminal_below(count)
+  local width = vim.api.nvim_win_get_width(0)
+  vim.cmd(width - 50 .. 'vsplit')
+  vim.cmd('wincmd l')
+  for i=1,count do
+    vim.cmd('terminal')
+    if i>1 then
+      vim.cmd('split')
+      vim.cmd('wincmd j')
+      vim.cmd('terminal')
+    end
+  end
 end
 
-vim.api.nvim_create_user_command('TestABCD', function (args)
-  terminal_below(2)
+vim.api.nvim_create_user_command('VTerm', function (args)
+  v_terminal_below(1)
+  end, {nargs="*"})
+
+vim.api.nvim_create_user_command('VTerm2', function (args)
+  v_terminal_below(2)
   end, {nargs="*"})
 
 return {
