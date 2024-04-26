67b7ab7c5063375bd6a0982fe7b47cbc82c3ea16
Ty Davis <tdavis@barnesaero.com>
2024-04-26 09:52:11 -0600

    Working on harpoon configuration

--- init.lua
+++ init.lua
@@ -582,6 +582,7 @@ require('which-key').register {
   ['<leader>d'] = { name = '[D]ocument', _ = 'which_key_ignore' },
   ['<leader>g'] = { name = '[G]it', _ = 'which_key_ignore' },
   ['<leader>h'] = { name = 'Git [H]unk', _ = 'which_key_ignore' },
+  ['<leader>p'] = { name = 'Har[P]oon', _ = 'which_key_ignore' },
   ['<leader>r'] = { name = '[R]ename', _ = 'which_key_ignore' },
   ['<leader>s'] = { name = '[S]earch', _ = 'which_key_ignore' },
   ['<leader>t'] = { name = '[T]oggle', _ = 'which_key_ignore' },
--- lua/custom/plugins/harpoon.lua
+++ lua/custom/plugins/harpoon.lua
@@ -1,5 +1,34 @@
+local conf = require("telescope.config").values
+
+local function toggle_telescope(harpoon_files)
+    local file_paths = {}
+    for _, item in ipairs(harpoon_files.items) do
+	table.insert(file_paths, item.value)
+    end
+
+    require("telescope.pickers").new({}, {
+	prompt_title = "Harpoon",
+	finder = require("telescope.finders").new_table({
+	    results = file_paths,
+	}),
+	previewer = conf.file_previewer({}),
+	sorter = conf.generic_sorter({}),
+    }):find()
+end
+
+
 return {
     "ThePrimeagen/harpoon",
     branch = "harpoon2",
-    dependencies = { "nvim-lua/plenary.nvim" }
+    dependencies = { "nvim-lua/plenary.nvim" },
+    config = function ()
+	local harpoon = require("harpoon")
+	harpoon:setup()
+
+
+    end,
+    keys = {
+	{ "<leader>pf", function() require("harpoon"):list():add() end, desc = "Harpoon [F]ile" },
+	{ "<leader>pl", function() toggle_telescope(require("harpoon"):list()) end, desc = "Show Harpoon [L]ist" },
+    }
 }
