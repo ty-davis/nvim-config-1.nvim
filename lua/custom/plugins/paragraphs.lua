2e1bd10540db5dba36354ce92174183191b54f3b
Ty Davis <tdavis@barnesaero.com>
2024-05-31 10:08:40 -0600

    Preserves indentation now and in visual mode will only format what you've selected

--- lua/custom/plugins/paragraphs.lua
+++ lua/custom/plugins/paragraphs.lua
@@ -1,7 +1,10 @@
-local single_line_command = '"050lf\\<space>r\\<CR>"'
+local single_line_command = '"_50lf\\<space>r\\<CR>"'
 
-local function fix_paragraph()
-  vim.api.nvim_command('normal vipJ')
+local function fix_paragraph(norm)
+  if norm then
+    vim.api.nvim_command('normal vip')
+  end
+  vim.api.nvim_command('normal J')
   for _ = 1, 100 do
     -- vim.api.nvim_command('normal 050lf r')
     vim.api.nvim_command('execute "normal" ' .. single_line_command)
@@ -10,7 +13,8 @@ local function fix_paragraph()
 end
 
 
-vim.keymap.set('n', '<C-n>', function() fix_paragraph() end, { noremap = true, desc = "Reformat paragraph"})
+vim.keymap.set('n', '<C-n>', function() fix_paragraph(true) end, { noremap = true, desc = "Reformat paragraph"})
+vim.keymap.set('v', '<C-n>', function() fix_paragraph(false) end, { noremap = true, desc = "Reformat selection"})
 
 return {
   ""
