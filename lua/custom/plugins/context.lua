624d96532c20f25ff16f42dc86cd94b8c31ff8d7
Ty Davis <tydavisxc2019@gmail.com>
2026-06-14 14:56:05 -0600

    Added keymap for contexttoggle

--- lua/custom/plugins/context.lua
+++ lua/custom/plugins/context.lua
@@ -2,5 +2,8 @@ return {
   repo = Gh 'wellle/context.vim',
   setup = function()
     vim.g.context_enabled = 0
+    
+    -- some keymaps or something
+    vim.keymap.set('n', '<leader>ct', '<cmd>ContextToggle<cr>')
   end
 }
